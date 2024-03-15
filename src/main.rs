f32f575c242dae9a5844c68f9062b059872aa49e
Jakub Janků <janku.jakub.jj@gmail.com>
2024-03-15 19:03:51 +0100

    parse cmd args

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,12 @@ mod io;
 mod types;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        println!("Usage: vw-passat INPUT");
+        return;
+    }
+    let mut input = std::fs::File::open(&args[1]).unwrap();
+
+    let problem = io::read_problem(&mut input);
 }
