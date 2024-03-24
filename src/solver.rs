ce43178fd238b47141b5f0f8aa4afbdf7a24268e
Jakub Janků <janku.jakub.jj@gmail.com>
2024-03-24 16:31:51 +0100

    add initial cdcl impl
    Likely contains some issues.

--- src/solver.rs
+++ src/solver.rs
@@ -10,42 +10,74 @@ fn to_var(lit: Lit) -> Var {
     lit.abs() as Var
 }
 
+#[derive(Clone, Copy, Debug)]
+enum Reason {
+    Decision,
+    Propagation { i_clause: usize },
+}
+
+#[derive(Clone)]
+struct VarData {
+    value: bool,
+    level: usize,
+    reason: Reason,
+}
+
 struct Assignment {
-    values: Vec<Option<bool>>,
+    data: Vec<Option<VarData>>,
     trail: Vec<Lit>,
-    decisions: Vec<usize>,
+    levels: Vec<usize>,
 }
 
 impl Assignment {
     fn new(var_count: usize) -> Self {
         Self {
-            values: vec![None; var_count + 1],
+            data: vec![None; var_count + 1],
             trail: vec![],
-            decisions: vec![],
+            levels: vec![],
         }
     }
 
     fn eval(&self, lit: Lit) -> Option<bool> {
-        self.values[to_var(lit)].map(|val| val == lit.is_positive())
+        self.data[to_var(lit)]
+            .as_ref()
+            .map(|data| data.value == lit.is_positive())
     }
 
-    fn assign(&mut self, lit: Lit) {
-        self.values[to_var(lit)] = Some(lit.is_positive());
+    fn set(&mut self, lit: Lit, reason: Reason) {
         self.trail.push(lit);
-    }
 
-    fn decide(&mut self, lit: Lit) {
-        self.assign(lit);
-        self.decisions.push(self.trail.len() - 1);
-    }
-
-    fn backtrack(&mut self) {
-        let dec = self.decisions.pop().unwrap();
-        for lit in self.trail.drain(dec + 1..) {
-            self.values[to_var(lit)] = None;
+        if let Reason::Decision = reason {
+            self.levels.push(self.trail.len() - 1);
         }
-        let lit = self.trail.pop().unwrap();
-        self.assign(-lit);
+
+        let data = VarData {
+            value: lit.is_positive(),
+            level: self.last_level(),
+            reason,
+        };
+        self.data[to_var(lit)] = Some(data);
+    }
+
+    fn level(&self, lit: Lit) -> Option<usize> {
+        self.data[to_var(lit)].as_ref().map(|data| data.level)
+    }
+
+    fn reason(&self, lit: Lit) -> Option<Reason> {
+        self.data[to_var(lit)].as_ref().map(|data| data.reason)
+    }
+
+    fn last_level(&self) -> usize {
+        self.levels.len()
+    }
+
+    /// Revert all changes at `level` (incl.) and above.
+    fn backtrack(&mut self, level: usize) {
+        self.levels.drain(level..);
+        let i = self.levels.pop().unwrap_or(0);
+        for lit in self.trail.drain(i..) {
+            self.data[to_var(lit)] = None;
+        }
     }
 }
 
@@ -90,34 +122,42 @@ impl Solver {
     pub fn new(problem: Problem) -> Self {
         let Problem { var_count, clauses } = problem;
 
-        let mut watched = LitMap::<Vec<usize>>::new(var_count);
-        for (i, clause) in clauses.iter().enumerate() {
-            if let [lit0, lit1, ..] = clause[..] {
-                watched[lit0].push(i);
-                watched[lit1].push(i);
-            }
+        let mut solver = Solver {
+            clauses: Vec::with_capacity(clauses.len()),
+            assignment: Assignment::new(var_count),
+            watched: LitMap::<Vec<usize>>::new(var_count),
+            prop_head: 0,
+        };
+
+        for clause in clauses {
+            solver.add(clause);
         }
 
-        Solver {
-            clauses,
-            assignment: Assignment::new(var_count),
-            watched,
-            prop_head: 0,
+        solver
+    }
+
+    fn add(&mut self, clause: Clause) -> usize {
+        let i = self.clauses.len();
+        if let [lit0, lit1, ..] = clause[..] {
+            self.watched[lit0].push(i);
+            self.watched[lit1].push(i);
         }
+        self.clauses.push(clause);
+        i
     }
 
     fn choose_var(&self) -> Option<Var> {
         // TODO: add iter() to Assignment?
         self.assignment
-            .values
+            .data
             .iter()
             .enumerate()
             .skip(1)
-            .find(|(_, val)| val.is_none())
+            .find(|(_, data)| data.is_none())
             .map(|(var, _)| var)
     }
 
-    fn propagate(&mut self) -> bool {
+    fn propagate(&mut self) -> Option<usize> {
         while let Some(lit) = self.assignment.trail.get(self.prop_head) {
             let lit = -lit;
 
@@ -158,10 +198,11 @@ impl Solver {
                 if self.assignment.eval(clause[0]).is_none() {
                     // unit clause
                     let unit_lit = clause[0];
-                    self.assignment.assign(unit_lit);
+                    self.assignment
+                        .set(unit_lit, Reason::Propagation { i_clause: c });
                 } else {
                     // conflict
-                    return false;
+                    return Some(c);
                 }
 
                 i += 1;
@@ -170,15 +211,73 @@ impl Solver {
             self.prop_head += 1;
         }
 
-        true
+        None
+    }
+
+    fn analyze(&self, i_conflict: usize) -> (Clause, usize) {
+        let mut learnt = self.clauses[i_conflict].clone();
+        let last_level = self.assignment.last_level();
+
+        let mut i_trail = self.assignment.trail.len();
+        let i_assert = loop {
+            let mut iter = learnt
+                .iter()
+                .enumerate()
+                .filter(|(_, &lit)| self.assignment.level(lit).unwrap() >= last_level);
+            // FIXME: can it be None?
+            let (i, _) = iter.next().unwrap();
+            if iter.next().is_none() {
+                break i;
+            }
+
+            i_trail -= 1;
+            let on_lit = self.assignment.trail[i_trail];
+
+            let i_reason = match self.assignment.reason(on_lit).unwrap() {
+                Reason::Propagation { i_clause } => i_clause,
+                Reason::Decision => unreachable!(),
+            };
+            let reason = &self.clauses[i_reason];
+            debug_assert!(reason.contains(&on_lit));
+
+            let len_before = learnt.len();
+            learnt.retain(|&lit| lit != -on_lit);
+            if learnt.len() != len_before {
+                // learnt contained -on_lit, finish the resolution
+                learnt.extend(reason.iter().filter(|&&lit| lit != on_lit));
+                // need to dedup to correctly determine #lits at a given level
+                learnt.sort();
+                learnt.dedup();
+            }
+        };
+
+        learnt.swap(0, i_assert);
+
+        let backtrack_level = if learnt.len() == 1 {
+            if self.assignment.last_level() > 0 {
+                1
+            } else {
+                self.assignment.eval(learnt[0]).unwrap() as usize
+            }
+        } else {
+            learnt[1..]
+                .iter()
+                .map(|&lit| self.assignment.level(lit).unwrap() + 1)
+                .max()
+                .unwrap()
+        };
+
+        (learnt, backtrack_level)
     }
 
     pub fn solve(&mut self) -> Solution {
-        for clause in &self.clauses {
+        for (i, clause) in self.clauses.iter().enumerate() {
             match clause[..] {
                 [] => return Solution::Unsat,
                 [lit] => match self.assignment.eval(lit) {
-                    None => self.assignment.assign(lit),
+                    None => self
+                        .assignment
+                        .set(lit, Reason::Propagation { i_clause: i }),
                     Some(false) => return Solution::Unsat,
                     Some(true) => (),
                 },
@@ -186,19 +285,27 @@ impl Solver {
             }
         }
 
-        if !self.propagate() {
+        if let Some(_i_conflict) = self.propagate() {
             return Solution::Unsat;
         }
 
         while let Some(var) = self.choose_var() {
-            self.assignment.decide(var as Lit);
+            self.assignment.set(var as Lit, Reason::Decision);
 
-            while !self.propagate() {
-                if self.assignment.decisions.is_empty() {
+            while let Some(i_conflict) = self.propagate() {
+                let (learnt, level) = self.analyze(i_conflict);
+
+                if level == 0 {
                     return Solution::Unsat;
                 }
-                self.assignment.backtrack();
-                self.prop_head = std::cmp::min(self.prop_head, self.assignment.trail.len() - 1);
+                self.assignment.backtrack(level);
+                self.prop_head = std::cmp::min(self.prop_head, self.assignment.trail.len());
+
+                let lit_assert = learnt[0];
+                // FIXME: the watch initialization may be incorrect here
+                let i_clause = self.add(learnt);
+                self.assignment
+                    .set(lit_assert, Reason::Propagation { i_clause });
             }
         }
 
@@ -219,7 +326,25 @@ pub fn verify(clauses: &[Clause], model: &[Lit]) -> bool {
 mod tests {
     use crate::types::{Clause, Problem, Solution};
 
-    use super::{verify, Solver};
+    use super::{verify, Assignment, Reason, Solver};
+
+    #[test]
+    fn assignment() {
+        let mut ass = Assignment::new(2);
+
+        assert_eq!(ass.last_level(), 0);
+
+        ass.set(1, Reason::Decision);
+        ass.set(-2, Reason::Propagation { i_clause: 0 });
+
+        assert_eq!(ass.last_level(), 1);
+        assert_eq!(ass.level(1), Some(1));
+        assert_eq!(ass.level(2), Some(1));
+
+        ass.backtrack(1);
+        assert_eq!(ass.eval(2), None);
+        assert_eq!(ass.eval(1), None);
+    }
 
     fn check(clauses: &[Clause], sat: bool) {
         let problem = Problem {
@@ -239,9 +364,34 @@ mod tests {
 
     #[test]
     /// Formulas from the lecture.
-    fn basic() {
+    fn basic_sat() {
         let clauses = vec![vec![1, 2], vec![-1, 2], vec![-1, -2, 3], vec![-1, -2, -3]];
         check(&clauses, true);
+
+        let clauses = vec![
+            vec![-1, -2, 3],
+            vec![2, -1, 3],
+            vec![1, -2, 3],
+            vec![-3, 4, 5],
+            vec![-3, 4, -5],
+            vec![-3, -4, 5],
+            vec![-3, -4, -5],
+        ];
+        check(&clauses, true);
+    }
+
+    #[test]
+    fn basic_unsat() {
+        let clauses = vec![
+            vec![1, 2],
+            vec![-2, 3],
+            vec![-2, -3],
+            vec![-1, -2, -4],
+            vec![-1, 2, -4],
+            vec![-1, 2, 4],
+        ];
+
+        check(&clauses, false);
     }
 
     #[test]
